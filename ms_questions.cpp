d0024071e21ab09bc7af30b04c44562634469ac2
Magdalena Glanowska <magda.glanowska@gmail.com>
2017-01-11 18:35:33 +0100

    added tasks

---
+++ ms_questions.cpp
@@ -0,0 +1,23 @@
+// anagram
+
+// reverse words in a string
+
+// You have a string input with text, where space between words was removed 
+// and method bool IsWord(string word) returning true if the string given is a word. 
+// Return the sentence with restored space between words.
+
+// You have a method bool Perform(string inputA, string inputB, string Validation) 
+// returning true if Validation string contains both inputA and inputB (can be interleaved)... 
+// for example for Perform("ABC","XYZ","AXYBCZ") return true;
+
+// You have Ackermann function. I am actually not sure what the question was supposed to be,
+// but the interviewer asked about how high m,n you can input not to end with stack overflow error.
+
+// Code method bool IsPrimeNumber(int candidate)
+
+//You have a linked list. Determine whether there is a loop in the linked list. 
+// Determine first cell in the linked list that is part of the loop.
+
+// difference between process and thread
+
+// Given 3 sorted arrays. Find the common element of all the arrays in O(N) time and no extra space. If there is none, return false.
---
+++ rotate_matrix.cpp
@@ -0,0 +1,16 @@
+// Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes,
+// write a method to rotate the image by 90 degrees. 
+// Can you do this in place?
+
+#include <iostream>
+
+void rotateMatrix(int matrix[][]) {
+
+}
+
+int main()
+{
+	std::cout << "rotateMatrix test:" << std::endl;
+	int test[][] = { {1, 2}, {3, 4} };
+	rotateMatrix(test);
+}
